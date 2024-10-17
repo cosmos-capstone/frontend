45972f6322e37c728ca3e2f9f2fea89d97cb6198
Ahn Sukyum <ahn.avocado@gmail.com>
2024-10-17 23:19:12 +0900

    Fix : Deleted unused Sidebar code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './sideBar';
+
 import DashboardPage from './pages/DashboardPage';
 import PortfolioPage from './pages/PortfolioPage';
 // import DataRegistrationPage from ' ./pages/DataRegistrationPage';
