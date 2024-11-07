ffe04fb26c12da15db83f12f2496d24b49ba2fd4
Youngho Cho <dominoyh5@gmail.com>
2024-11-07 17:57:35 +0900

    update: Dashboard.tsx to tailwind

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -2,58 +2,28 @@ import { CSSProperties } from 'react';
 
 export default function Dashboard() {
   return (
-    <div style={styles.dashboard}>
-      <div style={styles.overview}>
-        <h2>Overview</h2>
-        <div style={styles.statistics}>
-          <div style={styles.statItem}>
+    <div className="m-8 bg-white p-8 mb-5 rounded-lg shadow-md">
+      <div className="mb-3">
+        <h2 className="text-2xl font-semibold">Overview</h2>
+        <div className="flex justify-between mt-4">
+          <div className="text-center bg-gray-100 p-4 rounded-lg w-1/4 shadow-sm">
             <div>총 자산</div>
-            <div>$50,000</div>
+            <div className="font-bold">$50,000</div>
           </div>
-          <div style={styles.statItem}>
+          <div className="text-center bg-gray-100 p-4 rounded-lg w-1/4 shadow-sm">
             <div>주식</div>
-            <div>$100,000</div>
+            <div className="font-bold">$100,000</div>
           </div>
-          <div style={styles.statItem}>
+          <div className="text-center bg-gray-100 p-4 rounded-lg w-1/4 shadow-sm">
             <div>ETF</div>
-            <div>$500,000</div>
+            <div className="font-bold">$500,000</div>
           </div>
-          <div style={styles.statItem}>
+          <div className="text-center bg-gray-100 p-4 rounded-lg w-1/4 shadow-sm">
             <div>채권</div>
-            <div>$500,000</div>
+            <div className="font-bold">$500,000</div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 }
-
-const styles: { [key: string]: CSSProperties } = {
-  dashboard: {
-    backgroundColor: '#fff',
-    padding: '20px',
-    marginBottom: '20px',
-    borderRadius: '8px',
-    boxShadow: '0 1px 4px rgba(0, 0, 0, 0.1)',
-  },
-  overview: {
-    marginBottom: '10px',
-  },
-  statistics: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  statItem: {
-    textAlign: 'center',
-    backgroundColor: '#f1f3f6',
-    padding: '15px',
-    borderRadius: '8px',
-    width: '22%',
-    boxShadow: '0 1px 3px rgba(0, 0, 0, 0.1)',
-  },
-  chart: {
-    marginTop: '20px',
-    // 여기에 차트 관련 스타일 추가 가능
-  },
-};
