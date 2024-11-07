cfda8cd668260fec2809e24338a77dd3efab25cb
Youngho Cho <dominoyh5@gmail.com>
2024-11-07 16:31:23 +0900

    update: formattedData order

--- app/components/PieChart.tsx
+++ app/components/PieChart.tsx
@@ -15,15 +15,15 @@ const PieChart: React.FC<PieChartProps> = ({ data }) => {
   }
 
   const formattedData = [
+    { name: '현금', value: parseFloat(data.cash) },
+    { name: '예금', value: parseFloat(data.deposit) },
+    { name: '금', value: parseFloat(data.gold) },
     { name: '한국 주식', value: parseFloat(data.korean_stock) },
     { name: '미국 주식', value: parseFloat(data.american_stock) },
     { name: '한국 채권', value: parseFloat(data.korean_bond) },
     { name: '미국 채권', value: parseFloat(data.american_bound) },
     { name: '펀드', value: parseFloat(data.fund) },
     { name: '원자재', value: parseFloat(data.commodity) },
-    { name: '금', value: parseFloat(data.gold) },
-    { name: '예금', value: parseFloat(data.deposit) },
-    { name: '현금', value: parseFloat(data.cash) },
   ];
 
   const chartData = {
