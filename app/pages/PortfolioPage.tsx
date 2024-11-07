facee07cd31e4027917b51b38315b4c610f027f7
Youngho Cho <dominoyh5@gmail.com>
2024-11-07 16:06:00 +0900

    change: css to tailwind

--- app/pages/PortfolioPage.tsx
+++ app/pages/PortfolioPage.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { CSSProperties } from 'react';
-// import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 
 import PieChart from '../components/PieChart';
 import { ChartData } from 'chart.js';
@@ -46,72 +45,31 @@ const proposedData: ChartData<'pie'> = {
 
 const PortfolioAnalysis = () => {
   return (
-    <div style={styles.container}>
-      <h2 style={styles.title}>포트폴리오 분석</h2>
-      <div style={styles.portfolioSection}>
-        <div style={styles.portfolio}>
-          <h3 style={styles.subtitle}>기존 포트폴리오</h3>
-            <PieChart data={existingData} />
-          <div style={styles.details}>sharp ratio<br/>1년 과서 수익률<br/>MDD</div>
+    <div className="p-8 bg-gray-100 font-sans">
+      <h2 className="text-center mb-10 font-bold text-2xl">포트폴리오 분석</h2>
+      <div className="flex justify-around">
+        <div className="w-2/5 bg-gray-200 rounded-lg p-8 shadow-lg text-center">
+          <h3 className="mb-5 font-bold text-xl">기존 포트폴리오</h3>
+          <PieChart data={existingData} />
+          <div className="mt-8 bg-white p-4 rounded-lg text-left leading-relaxed">
+            <p>sharp ratio</p>
+            <p>1년 과서 수익률</p>
+            <p>MDD</p>
+          </div>
         </div>
 
-        <div style={styles.portfolio}>
-          <h3 style={styles.subtitle}>제안 포트폴리오</h3>
-            <PieChart data={proposedData} />
-          <div style={styles.details}>이렇게 추천하는 이유<br/>예상 수익률<br/>MDD</div>
+        <div className="w-2/5 bg-gray-200 rounded-lg p-8 shadow-lg text-center">
+          <h3 className="mb-5 font-bold text-xl">제안 포트폴리오</h3>
+          <PieChart data={proposedData} />
+          <div className="mt-8 bg-white p-4 rounded-lg text-left leading-relaxed">
+            <p>이렇게 추천하는 이유</p>
+            <p>예상 수익률</p>
+            <p>MDD</p>
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
-const styles: { [key: string]: CSSProperties } = {
-  container: {
-    // padding: '20px',
-    backgroundColor: '#f8f9fa',
-    fontFamily: 'Arial, sans-serif',
-  },
-  title: {
-    textAlign: 'center',
-    marginBottom: '40px',
-    fontWeight: 'bold',
-    fontSize: '24px',
-  },
-  portfolioSection: {
-    display: 'flex',
-    justifyContent: 'space-around',
-  },
-  portfolio: {
-    width: '45%',
-    backgroundColor: '#e9ecef',
-    borderRadius: '8px',
-    padding: '20px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    position: 'relative',
-    textAlign: 'center',
-  },
-  subtitle: {
-    marginBottom: '20px',
-    fontWeight: 'bold',
-    fontSize: '18px',
-  },
-  totalValue: {
-    position: 'absolute',
-    top: '140px',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    fontWeight: 'bold',
-    fontSize: '24px',
-    color: '#000',
-  },
-  details: {
-    marginTop: '30px',
-    backgroundColor: '#fff',
-    padding: '10px',
-    borderRadius: '8px',
-    textAlign: 'left',
-    lineHeight: '1.6',
-  },
 };
 
 export default PortfolioAnalysis;
