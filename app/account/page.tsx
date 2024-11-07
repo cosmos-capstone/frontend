66f68876027c5f7be52a54e24a28feea84aac0d6
Youngho Cho <dominoyh5@gmail.com>
2024-11-07 19:33:31 +0900

    update: account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Account: React.FC = () => {
   return (
-    <div>
-      <h1>Account Page</h1>
-      <p>Welcome to your account!</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Account Page</h1>
+      <p className="text-lg text-gray-600">This page is under development. Welcome to your account!</p>
     </div>
   );
 };
