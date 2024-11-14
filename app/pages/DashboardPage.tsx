c109c5d902eb68674fbf862a23bba640c4412f44
Youngho Cho <dominoyh5@gmail.com>
2024-11-14 18:19:02 +0900

    update: DashboardPage tailwind

--- app/pages/DashboardPage.tsx
+++ app/pages/DashboardPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import SankeyChartWrapper from '../components/GraphWrapper';
 import Dashboard from '../components/Dashboard';
-import styles from '../styles/page.module.css';
 import OptionSelector from '../components/OptionBoard';
 
 export default function Home() {
   return (
     <>
       <Dashboard />
-      <div className={styles.graphContent}>
+      <div className="flex flex-row flex-1 p-5 overflow-y-auto m-8 bg-gradient-to-br from-gray-100 to-white p-10 rounded-3xl shadow-2xl">
         <SankeyChartWrapper />
         <OptionSelector />
       </div>
