0719990bbe2df34e90de1ac3f5fcf71052781579
Youngho Cho <dominoyh5@gmail.com>
2024-11-14 18:38:38 +0900

    fix: tailwind

--- app/pages/DashboardPage.tsx
+++ app/pages/DashboardPage.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Dashboard />
-      <div className="flex flex-row flex-1 p-5 overflow-y-auto m-8 bg-gradient-to-br from-gray-100 to-white p-10 rounded-3xl shadow-2xl">
+      <div className="flex flex-row p-5 overflow-y-auto m-8 bg-gradient-to-br from-gray-100 to-white p-10 rounded-3xl shadow-2xl">
         <SankeyChartWrapper />
         <OptionSelector />
       </div>
