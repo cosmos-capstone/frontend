5ba2f5def5193a7cbf5712804f7b26b5219534cb
jse <73538772+jse8406@users.noreply.github.com>
2024-11-15 10:07:43 +0900

    CHORE : change layout

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -10,26 +10,25 @@ interface PortfolioData {
   [key: string]: string;
 }
 
+const assetNameMap = {
+  "korean_stock": "한국 주식",
+  "american_stock": "미국 주식",
+  "korean_bond": "한국 채권",
+  "american_bond": "미국 채권",
+  "commodity": "원자재",
+  "gold": "금",
+  "deposit": "예금",
+  "savings_account": "저축 계좌",
+  "cash": "현금"
+};
+
 const Portfolio = () => {
   const [existingData, setExistingData] = useState<PieChartData | null>(null);
   const [proposedData, setProposedData] = useState<PieChartData | null>(null);
   const [topAssets, setTopAssets] = useState<{ name: string; value: string }[]>([]);
   const [sharpeData, setSharpeData] = useState<SharpeRatioData | null>(null);
+  const [stocksData, setStocks] = useState<{ name: string; value: string }[]>([]);
 
-  // const [stocks, setStocks] = useState<{ name: string; value: string }[]>([]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const assetNameMap = {
-    "korean_stock": "한국 주식",
-    "american_stock": "미국 주식",
-    "korean_bond": "한국 채권",
-    "american_bond": "미국 채권",
-    "commodity": "원자재",
-    "gold": "금",
-    "deposit": "예금",
-    "savings_account": "저축 계좌",
-    "cash": "현금"
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,12 +39,15 @@ const Portfolio = () => {
         const reb_response = await fetch('https://cosmos-backend.cho0h5.org/transaction/rebalancing');
         const reb_data = await reb_response.json();
 
-        // const stock_response = await fetch('https://cosmos-backend.cho0h5.org/transaction/asset');
-        // const stock_data = await stock_response.json();
+
+        // 아직 사용 안함
+        const stock_response = await fetch('https://cosmos-backend.cho0h5.org/transaction/asset');
+        const stock_data = await stock_response.json();
 
         const portfolioData: PieChartData = por_data.data;
         const rebalancingData: PieChartData = reb_data.data;
-
+        console.log(stocksData);
+        
         const sharpe_ratios: SharpeRatioData = {
           korean_stock: 0.6,
           american_stock: 0.8,
@@ -59,8 +61,9 @@ const Portfolio = () => {
         setSharpeData(sharpe_ratios);
         setExistingData(portfolioData);
         setProposedData(rebalancingData);
-        // setStocks(stock_data);
-
+        setStocks(stock_data);
+        console.log(stock_data);
+        
         // 상위 3개의 자산 항목을 추출하여 상태에 저장
         const sortedData = Object.entries(por_data.data as PortfolioData)
           .map(([name, value]) => ({
@@ -78,7 +81,8 @@ const Portfolio = () => {
     };
 
     fetchData();
-  }, [assetNameMap]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -93,13 +97,40 @@ const Portfolio = () => {
           <div className="w-2/5 bg-gray-200 rounded-lg p-8 shadow-lg text-center">
             <h3 className="mb-5 font-bold text-3xl">제안 포트폴리오</h3>
             {proposedData ? <PieChart data={proposedData} /> : <div>Loading...</div>}
+            {/* <span className='font-semibold'>샤프 지수를 기반으로</span> */}
           </div>
         </div>
         <h2 className="text-center mt-20 font-bold text-3xl">포트폴리오 분석</h2>
       </div>
 
-      {/* 상위 3개의 자산을 표시하는 카드 */}
-      <div className="p-8 flex justify-center items-center bg-white">
+      
+
+      <div className="p-8 flex items-center bg-white" style={{ height: '500px' }}>
+        <p className="text-2xl font-semibold mb-1 ml-20">각 자산들의 샤프 지수는 다음과 같아요 </p>
+
+      </div>
+
+          
+      <div className='items-center justify-centera\'>
+        <div className="p-8 flex bg-white ml-10" style={{ height: '500px', width: '700px' }}>
+            {sharpeData ? <BarChart data={sharpeData} /> : <div>Loading Sharpe Ratios...</div>}
+            
+            <div className='item-start' style={{width: '500px'}}>
+              <p className="text-lg font-semibold mb-1 ml-10" style={{width: '500px'}}><br /> <span className="text-2xl font-bold">샤프 지수란</span> <br /><br />
+              변동성 대비 수익률을 뜻해요 (초과 수익/표준 편차)
+              <br />
+              <br />
+              <br />
+              <span className="text-gray-500">
+                샤프 지수가 높은 투자 자산일수록 변동성이 적고 수익률이 높아요
+                <br />안정적인 포트폴리오를 구성하기 위해서는 샤프 지수가 높은 자산을 많이 포함하는 것이 좋아요 
+                </span>
+              </p>
+            </div>          
+        </div>
+      </div>
+
+      <div className="p-8 flex justify-center items-center bg-white mb-40 mt-20">
         <div>
           <p className="text-xl font-bold mb-4 ">기존 <span style={{ color: '#3B82F6' }}>김코스</span>님의 자산은</p>
           <p className="text-lg font-medium mb-4">
@@ -113,7 +144,7 @@ const Portfolio = () => {
         </div>
 
       <div className="bg-gray-100 rounded-2xl shadow-lg p-8 w-96 text-center ml-60">
-        <p className="text-xl font-bold mb-4 text-blue-500">TOP3 자산 분포</p>
+        <p className="text-xl font-bold mb-02 text-blue-500">TOP3 자산 분포</p>
           {topAssets.map((asset, index) => (
               <div key={index} className="mb-4">
                   <p className="text-lg font-semibold mb-1">{asset.name}: {asset.value}%</p>
@@ -127,26 +158,7 @@ const Portfolio = () => {
           ))}
       </div>
       </div>
-
-      <div className="p-8 flex items-center bg-white" style={{ height: '500px' }}>
-        <p className="text-2xl font-semibold mb-1 ml-20">각 자산들의 샤프 지수는 다음과 같아요 </p>
-      </div>
-
-      <div className='items-center justify-center'>
-        <div className="p-8 flex items-center bg-white" style={{ height: '500px', width: '700px' }}>
-            {sharpeData ? <BarChart data={sharpeData} /> : <div>Loading Sharpe Ratios...</div>}
-
-            <div className='item-start' style={{width: '500px'}}>
-              <p className="text-lg font-semibold mb-1 ml-20" style={{width: '500px'}}>샤프 지수란 <br />
-              변동성 대비 수익률을 뜻해요
-              </p>
-              
-            </div>
-            
-            
-        </div>
-      </div>
-
+      
     </>
   );
 };
