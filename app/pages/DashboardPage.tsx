34bc10907038ce47c845c6c840c4eae78954194c
Youngho Cho <dominoyh5@gmail.com>
2024-11-16 17:30:29 +0900

    fix: add table, tbody to EditTransactionRow

--- app/pages/DashboardPage.tsx
+++ app/pages/DashboardPage.tsx
@@ -75,106 +75,110 @@ const EditTransactionRow = ({
   americanStocks,
   setModifiedTransactions
 }: EditTransactionRowProps) => (
-  <tr key={index} className="text-gray-600 bg-gray-50">
-    <td className="border-b py-2">
-      <input
-        type="datetime-local"
-        name="transaction_date"
-        value={formatDateForInput(transaction.transaction_date)}
-        onChange={(e) => handleInputChange(index, e)}
-        className="w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        required
-      />
-    </td>
-    <td className="border-b py-2">
-      <select
-        name="transaction_type"
-        value={transaction.transaction_type}
-        onChange={(e) => handleInputChange(index, e)}
-        className="w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        required
-      >
-        <option value="deposit">Deposit</option>
-        <option value="withdrawal">Withdrawal</option>
-        <option value="buy">Buy</option>
-        <option value="sell">Sell</option>
-      </select>
-    </td>
-    <td className="border-b py-2">
-      <select
-        name="asset_category"
-        value={transaction.asset_category}
-        onChange={(e) => handleInputChange(index, e)}
-        className={`w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 ${transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"
-          ? "bg-gray-200 opacity-60 cursor-not-allowed"
-          : ""
-          }`}
-        disabled={transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"}
-        required
-      >
-        <option value="korean_stock">Korean Stock</option>
-        <option value="american_stock">American Stock</option>
-        <option value="korean_bond">Korean Bond</option>
-        <option value="american_bond">American Bond</option>
-        <option value="commodity">Commodity</option>
-        <option value="gold">Gold</option>
-        <option value="deposit">Deposit</option>
-        <option value="savings">Savings Account</option>
-      </select>
-    </td>
-    <td className="border-b py-2">
-      {(transaction.asset_category !== "korean_stock" && transaction.asset_category !== "american_stock") && (
-        <input
-          type="text"
-          name="asset_name"
-          value={transaction.asset_name || ""}
-          onChange={(e) => handleInputChange(index, e)}
-          className={`w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 ${transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"
-            ? "bg-gray-200 opacity-60 cursor-not-allowed"
-            : ""
-            }`}
-          disabled={transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"}
-        />
-      )}
+  <table>
+    <tbody>
+      <tr key={index} className="text-gray-600 bg-gray-50">
+        <td className="border-b py-2">
+          <input
+            type="datetime-local"
+            name="transaction_date"
+            value={formatDateForInput(transaction.transaction_date)}
+            onChange={(e) => handleInputChange(index, e)}
+            className="w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            required
+          />
+        </td>
+        <td className="border-b py-2">
+          <select
+            name="transaction_type"
+            value={transaction.transaction_type}
+            onChange={(e) => handleInputChange(index, e)}
+            className="w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            required
+          >
+            <option value="deposit">Deposit</option>
+            <option value="withdrawal">Withdrawal</option>
+            <option value="buy">Buy</option>
+            <option value="sell">Sell</option>
+          </select>
+        </td>
+        <td className="border-b py-2">
+          <select
+            name="asset_category"
+            value={transaction.asset_category}
+            onChange={(e) => handleInputChange(index, e)}
+            className={`w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 ${transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"
+              ? "bg-gray-200 opacity-60 cursor-not-allowed"
+              : ""
+              }`}
+            disabled={transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"}
+            required
+          >
+            <option value="korean_stock">Korean Stock</option>
+            <option value="american_stock">American Stock</option>
+            <option value="korean_bond">Korean Bond</option>
+            <option value="american_bond">American Bond</option>
+            <option value="commodity">Commodity</option>
+            <option value="gold">Gold</option>
+            <option value="deposit">Deposit</option>
+            <option value="savings">Savings Account</option>
+          </select>
+        </td>
+        <td className="border-b py-2">
+          {(transaction.asset_category !== "korean_stock" && transaction.asset_category !== "american_stock") && (
+            <input
+              type="text"
+              name="asset_name"
+              value={transaction.asset_name || ""}
+              onChange={(e) => handleInputChange(index, e)}
+              className={`w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 ${transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"
+                ? "bg-gray-200 opacity-60 cursor-not-allowed"
+                : ""
+                }`}
+              disabled={transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"}
+            />
+          )}
 
-      {transaction.asset_category === "korean_stock" && (
-        <Select
-          options={koreanStocks}
-          onChange={(selectedOption) => handleAssetNameChange(index, selectedOption, setModifiedTransactions)}
-          className="w-full"
-        />
-      )}
-      {transaction.asset_category === "american_stock" && (
-        <Select
-          options={americanStocks}
-          onChange={(selectedOption) => handleAssetNameChange(index, selectedOption, setModifiedTransactions)}
-          className="w-full"
-        />
-      )}
-    </td>
-    <td className="border-b py-2">
-      <input
-        type="number"
-        name="quantity"
-        value={transaction.quantity}
-        onChange={(e) => handleInputChange(index, e)}
-        className={`w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 ${transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"
-          ? "bg-gray-200 opacity-60 cursor-not-allowed"
-          : ""
-          }`}
-        disabled={transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"}
-        required
-      />
-    </td>
-    <td className="border-b py-2">
-      <input
-        type="number"
-        name="transaction_amount"
-        value={transaction.transaction_amount}
-        onChange={(e) => handleInputChange(index, e)}
-        className="w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        required
-      />
-    </td>
-  </tr>
+          {transaction.asset_category === "korean_stock" && (
+            <Select
+              options={koreanStocks}
+              onChange={(selectedOption) => handleAssetNameChange(index, selectedOption, setModifiedTransactions)}
+              className="w-full"
+            />
+          )}
+          {transaction.asset_category === "american_stock" && (
+            <Select
+              options={americanStocks}
+              onChange={(selectedOption) => handleAssetNameChange(index, selectedOption, setModifiedTransactions)}
+              className="w-full"
+            />
+          )}
+        </td>
+        <td className="border-b py-2">
+          <input
+            type="number"
+            name="quantity"
+            value={transaction.quantity}
+            onChange={(e) => handleInputChange(index, e)}
+            className={`w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 ${transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"
+              ? "bg-gray-200 opacity-60 cursor-not-allowed"
+              : ""
+              }`}
+            disabled={transaction.transaction_type === "deposit" || transaction.transaction_type === "withdrawal"}
+            required
+          />
+        </td>
+        <td className="border-b py-2">
+          <input
+            type="number"
+            name="transaction_amount"
+            value={transaction.transaction_amount}
+            onChange={(e) => handleInputChange(index, e)}
+            className="w-full px-2 py-1 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            required
+          />
+        </td>
+      </tr>
+    </tbody>
+  </table>
 );
