571a0729d8f35c7d6b058ea65206c3718523a4a2
Ahn Sukyum <ahn.avocado@gmail.com>
2024-12-01 01:11:09 +0900

    ADD : Add original mockup data for debugging issue

--- app/pages/DashboardPage.tsx
+++ app/pages/DashboardPage.tsx
@@ -10,6 +10,8 @@ import { fetchTransactions } from '../utils/api';
 import { fetchStockData } from '../utils/api';
 import { handleAssetNameChange, handleInputChange } from '../utils/dataRegistration';
 import AssetTracker from '@/app/components/AssetTracker';
+import {TRANSACTION_DATA} from '@/app/data/transactionsMockup'
+
 
 export default function Home() {
   const [existingTransactions, setExistingTransactions] = useState<Transaction[]>([]);
@@ -33,7 +35,7 @@ export default function Home() {
     <div className="flex flex-col space-y-8 bg-gray-100">
       <Dashboard />
       <div className="flex flex-row p-6 m-8 bg-white rounded-2xl border border-gray-200">
-        <CustomFlowChart transactions={existingTransactions} />
+        <CustomFlowChart transactions={TRANSACTION_DATA} />
         <OptionSelector />
       </div>
       <div className="flex flex-row p-6 m-8 bg-white rounded-2xl border border-gray-200">
@@ -54,7 +56,7 @@ export default function Home() {
           <CustomFlowChart transactions={modifiedTransactions} />
         )}
       </div>
-      <AssetTracker transactionData={existingTransactions} />
+      <AssetTracker transactionData={TRANSACTION_DATA} />
     </div>
   );
 }
