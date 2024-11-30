0cbbe2b82ed944b8e0a0849c380930ba91d9407e
Ahn Sukyum <ahn.avocado@gmail.com>
2024-12-01 00:27:47 +0900

    FIX : Fixed data foramt and added date display to node

--- app/components/CustomFlowChart/Node.tsx
+++ app/components/CustomFlowChart/Node.tsx
@@ -22,8 +22,8 @@ export const Node = ({ node }: NodeProps) => {
     // };
 
     // const backgroundColor = getBackgroundColor(node.type);
-    const simpleLabel = `${node.asset_symbol}`;
-    const detailLabel = `${node.asset_symbol}\n${node.amount}주\n₩${node.value?.toLocaleString() ?? '0'}\n${node.date}`;
+    const simpleLabel = `${node.asset_symbol}\n${node.date.toLocaleDateString()}`;
+    const detailLabel = `${node.asset_symbol}\n${node.amount}주\n₩${node.value?.toLocaleString() ?? '0'}\n${node.date.toLocaleDateString()}`;
     const showLabel = node.state === 'before';
 
     return (
