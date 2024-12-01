b729aa1097ae6c6e0c488212eeedd90c853d83b4
jse <73538772+jse8406@users.noreply.github.com>
2024-12-01 17:13:02 +0900

    FIXED : basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/frontend/portfolio',
+    reactStrictMode: true,
+};
 
 export default nextConfig;
