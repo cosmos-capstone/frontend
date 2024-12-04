dd1b290e05ac4435facc2e7952de00444751a096
Youngho Cho <dominoyh5@gmail.com>
2024-12-04 20:45:13 +0900

    update: topbar (add logo and change title

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image'
+import favicon from './favicon.png'
 
 export default function TopBar() {
   return (
-    <div className="flex items-center justify-between px-5 py-3 border-b-2 border-blue-500 bg-white">
-      <span className="text-xl font-bold text-gray-800">Overview</span>
+    <div className="flex items-center justify-start px-5 py-3 border-b-2 border-blue-500 bg-white">
+      <Image
+        className="rounded-full mr-2"
+        src={favicon}
+        alt="Logo"
+        width={32}
+      />
+      <span className="text-xl font-bold text-gray-800">Cosmos</span>
     </div>
   );
 }
